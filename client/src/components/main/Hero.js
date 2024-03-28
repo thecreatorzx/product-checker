0d7a7d9dad0387c7da79fd9b9b380248019be632
thecreatorzx <saadyesican@gmail.com>
2024-03-28 11:54:35 +0530

    added in server

--- client/src/components/main/Hero.js
+++ client/src/components/main/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="pt-14">
-      <div className="hero flex justify-between items-center w-full h-96 px-5 mb-16 text-2xl text-center">
+      <div className="hero flex justify-around items-center w-full h-96 mb-16 text-2xl text-center">
         <h1 className="w-10 satisfy text-6xl text-gray-900 pl-16">
           Scan &nbsp;Products &nbsp;&nbsp;For &nbsp;&nbsp;&nbsp;Toxin
         </h1>
--- server/server.js
+++ server/server.js
@@ -2,28 +2,34 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
-const { connectMongoose , User } = require("./conn.js");
+const { connectMongoose, User } = require("./conn.js");
+const cors = require("cors");
 
 connectMongoose();
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "../client/public")));
-
+app.use(cors());
 
 app.get("/", (req, res) => {
-    // Send the 'index.html' file as the response
-    res.sendFile(path.join(__dirname, "../client/public", "index.html"));
-  });
-  
+  // Send the 'index.html' file as the response
+  res.send("<h1> Server is live </h1>");
+});
 
 app.post("/signup", (req, res) => {
-    const { name, email, password } = req.body;
-    // Process email and password (e.g., save to database)
-    console.log("Received name:", name);
-    console.log("Received email:", email);
-    console.log("Received password:", password);
-    // Send response back to client
-    res.send("Received email and password");
-  });
+  const { name, email, password } = req.body;
+  // Process email and password (e.g., save to database)
+  console.log("Received name:", name);
+  console.log("Received email:", email);
+  console.log("Received password:", password);
+  // Send response back to client
+  if (name) {
+    res
+      .status(200)
+      .json({ success: true, msg: "Received email and password:" });
+  }
+  if (!name) {
+    res.status(400).json({ success: false, msg: "bad request" });
+  }
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
