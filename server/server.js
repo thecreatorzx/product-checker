d62b6bb68e5fb36a5733104092ed400aa2388d50
thecreatorzx <saadyesican@gmail.com>
2024-03-29 11:21:02 +0530

    corrections

--- server/server.js
+++ server/server.js
@@ -21,11 +21,11 @@ app.post("/signup", async (req, res) => {
 
   if (user != null) {
     console.log("error", "User already exists");
-    res.status(403).json({ message: "User already exists" });
+    res.status(403).json({ msg: "User already exists" });
   } else {
     const newUser = await User.create(req.body);
     console.log("success", "Registered succesfully");
-    res.status(200).json({ message: "Signup successful" });
+    res.status(200).json({ msg: "Signup successful" });
   }
 });
 
@@ -35,15 +35,15 @@ app.post("/login", async (req, res) => {
   const user = await User.findOne({ email: email });
   if (user != null) {
     if (user.password == password) {
-      console.log("success", `welcome + ${user.username}`);
-      res.status(200).json({ message: `welcome + ${user.username}` });
+      console.log("success", ` welcome + ${user.username}`);
+      res.status(200).json({ msg: `welcome ${user.username} ` });
     } else {
-      console.log("error", "password does'nt matched");
-      res.status(400).json({ message: "password does'nt matched" });
+      console.log("error", "password doesn't match.");
+      res.status(400).json({ msg: "password doesn't match." });
     }
   } else {
     console.log("error", "username not found");
-    res.status(404).json({ message: "username not found" });
+    res.status(404).json({ msg: "username not found" });
   }
 
   // console.log("Received email:", email);
