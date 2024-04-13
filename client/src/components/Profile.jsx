2a9a55b9e6221f770201015859ed074155db332f
Raghav chawla <raghchawla.131@gmail.com>
2024-04-13 16:32:52 +0530

    docs how profile

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <>
+      <div className=" flex flex-col justify-center items-center h-screen">
+        <div className="image2"></div>
+        <div className=" flex flex-col justify-center items-center">
+          <h1>Name</h1>
+          <h1>User name</h1>
+          <h1>Email</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
